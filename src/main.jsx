ed29228f5e39cfac07bf08dc53a73b960ea9f80b
KMukendi10 <125656620+KMukendi10@users.noreply.github.com>
2026-08-05 13:47:46 +0200

    Integrate ProductsContext into application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ProductsProvider } from "./context/ProductsContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
+  </StrictMode>
+);
